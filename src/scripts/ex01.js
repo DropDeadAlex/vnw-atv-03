11eb1f2f910a92a61ce6333859ebf942bbc79b4d
Alex de Jesus <DropDeadAlex@msn.com>
2023-02-01 16:10:52 -0300

    add ex01

--- src/scripts/ex01.js
+++ src/scripts/ex01.js
@@ -1,1 +1,20 @@
-console.log("eaoeao");
+// 01- Crie um array que receba 5 itens e exiba no console.
+// 02- Utilize um método para adicionar um nome ao inicio do array.
+// 03- Utilize um método para remover o último nome do array.
+// 04- Utilize um método para adicionar dois nomes ao fim do array.
+// 05- Utilize um método para remover o primeiro nome do array.
+
+let array = ["İ", "€", "æ", "œ", "ø"]
+console.log(array);
+
+array.unshift("Abe")        //begin.add()
+console.log(array);
+
+array.pop()                 //last.del()
+console.log(array);
+
+array.push("Alli", "Elle")  //end.add()
+console.log(array);
+
+array.shift()               //first.del()
+console.log(array);
