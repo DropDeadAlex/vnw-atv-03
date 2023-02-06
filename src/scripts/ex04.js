fa444f92f3d8b55c1727ffc048c2d3e6cb67208a
Alex de Jesus <DropDeadAlex@msn.com>
2023-02-05 23:09:28 -0300

    new methods

--- src/scripts/ex04.js
+++ src/scripts/ex04.js
@@ -4,31 +4,11 @@
 // 12- Mostre no console o nome de um amigo de cada lista.
 
 const cadastro = [
-  {
-    nome: "Alix",
-    idade: 21,
-    telefone: "+55 21 3554-1420",
-    /** @type {string[]} */
-    amigos: []
-  },
-  {
-    nome: "Ena",
-    idade: 9,
-    telefone: "+55 21 3154-6420",
-    amigos: []
-  },
-  {
-    nome: "Vic",
-    idade: 11,
-    telefone: "+55 21 93554-1420",
-    amigos: []
-  },
-  {
-    nome: "Sol",
-    idade: 52,
-    telefone: "+55 65 98554-9420",
-    amigos: []
-  },
+  { nome: "Alix", idade: 21, telefone: "+55 21 3554-1420",
+    /** @type {string[]} */ amigos: [] },
+  { nome: "Ena", idade: 9,  telefone: "+55 21 3154-6420",  amigos: [] },
+  { nome: "Vic", idade: 11, telefone: "+55 21 93554-1420", amigos: [] },
+  { nome: "Sol", idade: 52, telefone: "+55 65 98554-9420", amigos: [] },
 ];
 
 
@@ -73,23 +53,53 @@ cadastro.forEach(({ amigos }, i) => {
     .map(j => `amg${(i * 4) + j}`)
   )
 })
-// ----------------------------------------------
 
+
+// Diferentes maneiras de mapear cadastro[]  -------------------
+// para exibir amigos no console -------------------------------
 
 // for...of com destructuring do objeto retornado
 for (const { nome, amigos } of cadastro)
   console.log(`Amigo de ${nome}: ${amigos[0]}`);
+;
 
 
 console.log("");    //  spacer
 
-
 // forEach capturando o index
 cadastro.forEach(({ nome, amigos }, i) =>
   console.log(`Amigo de ${nome}: ${amigos[i]}`)
-)
+);
+
+
+console.log("");    //  spacer
+
+cadastro
+  .map( ({ nome, amigos }) => `Amigo de ${nome}: ${amigos[0]}` )
+  .forEach(frase => console.log(frase))
+;
+
+
+console.log("");    //  spacer
+
+console.log(
+  cadastro
+    .map( ({ nome, amigos }) => `Amigo de ${nome}: ${amigos[0]}` )
+    .join("\n")
+);
+
+
+console.log("");    //  spacer
+
+console.log(
+  cadastro
+    .map( ({ nome, amigos }) => `Amigo de ${nome}: ${amigos[0]}` )
+    .reduce((blocoTxt, linha) => blocoTxt.concat(linha, "\n"), "")
+);
+
 
 
 console.log("");
-console.log(cadastro);
-console.log(Array(4).keys().next().done);
+// console.log(cadastro);
+// console.log(cadastro.values().next().value);
+globalThis.cadastro = cadastro
